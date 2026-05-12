f713eb92919cf0819ed5861ab236bbf45640fec1
Md Shafkat Hussain Tanvir <tanvir0604@gmail.com>
2026-05-12 10:21:33 +0600

    remove debugging code

--- src/nestauth.controller.ts
+++ src/nestauth.controller.ts
@@ -1,16 +1,16 @@
 import {
-    Request,
-    Controller,
-    Get,
-    Post,
-    UseGuards,
-    Body,
-    UnauthorizedException,
-    HttpStatus,
-    All,
-    BadRequestException,
-    UseFilters,
-    Inject,
+  Request,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Body,
+  UnauthorizedException,
+  HttpStatus,
+  All,
+  BadRequestException,
+  UseFilters,
+  Inject,
 } from "@nestjs/common";
 import { NestAuthService } from "./nestauth.service";
 import { NestAuthGoogleGuard } from "./nestauth-google.guard";
@@ -18,76 +18,72 @@ import { NestAuthFacebookGuard } from "./nestauth-facebook.guard";
 import { HttpExceptionFilter } from "./http-exception.filter";
 
 export function createDynamicController(
-    prefix: string,
-    nestAuthServiceToken: string,
-    localGuard: any,
+  prefix: string,
+  nestAuthServiceToken: string,
+  localGuard: any,
 ) {
-    @UseFilters(HttpExceptionFilter)
-    @Controller(prefix)
-    class NestAuthController {
-        constructor(
-            @Inject(nestAuthServiceToken)
-            readonly nestAuthService: NestAuthService,
-        ) {}
+  @UseFilters(HttpExceptionFilter)
+  @Controller(prefix)
+  class NestAuthController {
+    constructor(
+      @Inject(nestAuthServiceToken)
+      readonly nestAuthService: NestAuthService,
+    ) {}
 
-        @All()
-        async greetings(): Promise<string> {
-            return "Welcome to NestAuth. Please check our documentation for more information.";
-        }
-
-        @UseGuards(localGuard)
-        @Post("login")
-        async login(@Request() req: any): Promise<any> {
-            console.log("nestAuthServiceToken", nestAuthServiceToken);
-            return this.nestAuthService.login(req.user);
-        }
-
-        @Post("refresh-token")
-        refreshToken(@Body() params: { refresh_token: string }): Promise<any> {
-            if (!params.refresh_token) {
-                throw new BadRequestException(
-                    "Invalid or expired refresh token",
-                );
-            }
-            return this.nestAuthService.refreshToken(params.refresh_token);
-        }
-
-        @UseGuards(NestAuthGoogleGuard)
-        @Get("google")
-        async googleAuth(@Request() req: any): Promise<any> {}
-
-        @Get("google-redirect")
-        @UseGuards(NestAuthGoogleGuard)
-        googleAuthRedirect(@Request() req) {
-            if (!req.user) {
-                throw new UnauthorizedException("Unable to login with Google");
-            }
-            return this.nestAuthService.google(req.user);
-        }
-
-        @Get("/facebook")
-        @UseGuards(NestAuthFacebookGuard)
-        async facebookLogin(): Promise<any> {
-            return HttpStatus.OK;
-        }
-
-        @Get("/facebook-redirect")
-        @UseGuards(NestAuthFacebookGuard)
-        async facebookLoginRedirect(@Request() req): Promise<any> {
-            if (!req.user) {
-                throw new UnauthorizedException(
-                    "Unable to login with Facebook",
-                );
-            }
-            return this.nestAuthService.facebook(req.user);
-        }
-
-        @UseGuards(localGuard)
-        @Get("logout")
-        async logout(@Request() req: any) {
-            return req.logout();
-        }
+    @All()
+    async greetings(): Promise<string> {
+      return "Welcome to NestAuth. Please check our documentation for more information.";
     }
 
-    return NestAuthController;
+    @UseGuards(localGuard)
+    @Post("login")
+    async login(@Request() req: any): Promise<any> {
+      // console.log("nestAuthServiceToken", nestAuthServiceToken);
+      return this.nestAuthService.login(req.user);
+    }
+
+    @Post("refresh-token")
+    refreshToken(@Body() params: { refresh_token: string }): Promise<any> {
+      if (!params.refresh_token) {
+        throw new BadRequestException("Invalid or expired refresh token");
+      }
+      return this.nestAuthService.refreshToken(params.refresh_token);
+    }
+
+    @UseGuards(NestAuthGoogleGuard)
+    @Get("google")
+    async googleAuth(@Request() req: any): Promise<any> {}
+
+    @Get("google-redirect")
+    @UseGuards(NestAuthGoogleGuard)
+    googleAuthRedirect(@Request() req) {
+      if (!req.user) {
+        throw new UnauthorizedException("Unable to login with Google");
+      }
+      return this.nestAuthService.google(req.user);
+    }
+
+    @Get("/facebook")
+    @UseGuards(NestAuthFacebookGuard)
+    async facebookLogin(): Promise<any> {
+      return HttpStatus.OK;
+    }
+
+    @Get("/facebook-redirect")
+    @UseGuards(NestAuthFacebookGuard)
+    async facebookLoginRedirect(@Request() req): Promise<any> {
+      if (!req.user) {
+        throw new UnauthorizedException("Unable to login with Facebook");
+      }
+      return this.nestAuthService.facebook(req.user);
+    }
+
+    @UseGuards(localGuard)
+    @Get("logout")
+    async logout(@Request() req: any) {
+      return req.logout();
+    }
+  }
+
+  return NestAuthController;
 }
--- src/nestauth.module.ts
+++ src/nestauth.module.ts
@@ -14,115 +14,110 @@ import { createLocalStrategy } from "./nestauth-local.strategy";
 import { createLocalGuard } from "./nestauth-local.guard";
 
 @Module({
-    imports: [PassportModule, ConfigModule.forRoot({})],
+  imports: [PassportModule, ConfigModule.forRoot({})],
 })
 export class NestAuthModule {
-    static forRoot(options: NestAuthModuleOptions): DynamicModule {
-        const JwtSecretProvider: Provider = {
-            provide: "JWT_SECRET",
-            useValue: options.jwtSecret || "60s",
-        };
+  static forRoot(options: NestAuthModuleOptions): DynamicModule {
+    const JwtSecretProvider: Provider = {
+      provide: "JWT_SECRET",
+      useValue: options.jwtSecret || "60s",
+    };
 
-        const JwtExpiresInProvider: Provider = {
-            provide: "JWT_EXPIRES_IN",
-            useValue: options.jwtExpiresIn,
-        };
+    const JwtExpiresInProvider: Provider = {
+      provide: "JWT_EXPIRES_IN",
+      useValue: options.jwtExpiresIn,
+    };
 
-        const JwtRefreshTokenExpiresInProvider: Provider = {
-            provide: "JWT_REFRESH_TOKEN_EXPIRES_IN",
-            useValue: options.jwtRefreshTokenExpiresIn,
-        };
+    const JwtRefreshTokenExpiresInProvider: Provider = {
+      provide: "JWT_REFRESH_TOKEN_EXPIRES_IN",
+      useValue: options.jwtRefreshTokenExpiresIn,
+    };
 
-        const controllerPath = options.routePrefix
-            ? `${options.routePrefix.replace(/^\/|\/$/g, "")}/nestauth`
-            : "nestauth";
+    const controllerPath = options.routePrefix
+      ? `${options.routePrefix.replace(/^\/|\/$/g, "")}/nestauth`
+      : "nestauth";
 
-        const pathKey = controllerPath.replaceAll("/", "_").toUpperCase();
+    const pathKey = controllerPath.replaceAll("/", "_").toUpperCase();
 
-        const userServiceToken = `NEST_AUTH_USER_SERVICE_${pathKey}`;
-        const nestAuthServiceToken = `NEST_AUTH_SERVICE_${pathKey}`;
+    const userServiceToken = `NEST_AUTH_USER_SERVICE_${pathKey}`;
+    const nestAuthServiceToken = `NEST_AUTH_SERVICE_${pathKey}`;
 
-        const strategyName = `${pathKey}-local`;
+    const strategyName = `${pathKey}-local`;
 
-        const LocalStrategy = createLocalStrategy(
-            strategyName,
+    const LocalStrategy = createLocalStrategy(strategyName, userServiceToken);
+
+    const LocalGuard = createLocalGuard(strategyName);
+
+    // console.log("controllerPath", controllerPath);
+    // console.log("pathKey", pathKey);
+    // console.log("userServiceToken", userServiceToken);
+    // console.log("nestAuthServiceToken", nestAuthServiceToken);
+
+    // console.log("------------------------------------------");
+
+    const controller = createDynamicController(
+      controllerPath,
+      nestAuthServiceToken,
+      LocalGuard,
+    );
+
+    return {
+      module: NestAuthModule,
+      imports: [
+        JwtModule.registerAsync({
+          imports: [],
+          inject: [],
+          useFactory: async () => ({
+            secret: options.jwtSecret,
+            signOptions: {
+              expiresIn: options.jwtExpiresIn as StringValue | number,
+            },
+          }),
+        }),
+        forwardRef(() => options.UserModule),
+      ],
+      providers: [
+        {
+          provide: userServiceToken,
+          useExisting: options.UserService,
+        },
+        {
+          provide: nestAuthServiceToken,
+          useFactory: (
+            jwtService: JwtService,
+            userService: NestAuthInterface,
+            jwtExpiresIn: StringValue | number,
+            jwtRefreshTokenExpiresIn: StringValue | number,
+          ) =>
+            new NestAuthService(
+              jwtService,
+              userService,
+              jwtExpiresIn,
+              jwtRefreshTokenExpiresIn,
+            ),
+          inject: [
+            JwtService,
             userServiceToken,
-        );
+            "JWT_EXPIRES_IN",
+            "JWT_REFRESH_TOKEN_EXPIRES_IN",
+          ],
+        },
+        NestAuthJwtStrategy,
+        NestAuthGoogleStrategy,
+        NestAuthFacebookStrategy,
+        JwtSecretProvider,
+        JwtExpiresInProvider,
+        JwtRefreshTokenExpiresInProvider,
+        LocalStrategy,
+        LocalGuard,
 
-        const LocalGuard = createLocalGuard(strategyName);
-
-        console.log("controllerPath", controllerPath);
-        console.log("pathKey", pathKey);
-        console.log("userServiceToken", userServiceToken);
-        console.log("nestAuthServiceToken", nestAuthServiceToken);
-
-        console.log("------------------------------------------");
-
-        const controller = createDynamicController(
-            controllerPath,
-            nestAuthServiceToken,
-            LocalGuard,
-        );
-
-        return {
-            module: NestAuthModule,
-            imports: [
-                JwtModule.registerAsync({
-                    imports: [],
-                    inject: [],
-                    useFactory: async () => ({
-                        secret: options.jwtSecret,
-                        signOptions: {
-                            expiresIn: options.jwtExpiresIn as
-                                | StringValue
-                                | number,
-                        },
-                    }),
-                }),
-                forwardRef(() => options.UserModule),
-            ],
-            providers: [
-                {
-                    provide: userServiceToken,
-                    useExisting: options.UserService,
-                },
-                {
-                    provide: nestAuthServiceToken,
-                    useFactory: (
-                        jwtService: JwtService,
-                        userService: NestAuthInterface,
-                        jwtExpiresIn: StringValue | number,
-                        jwtRefreshTokenExpiresIn: StringValue | number,
-                    ) =>
-                        new NestAuthService(
-                            jwtService,
-                            userService,
-                            jwtExpiresIn,
-                            jwtRefreshTokenExpiresIn,
-                        ),
-                    inject: [
-                        JwtService,
-                        userServiceToken,
-                        "JWT_EXPIRES_IN",
-                        "JWT_REFRESH_TOKEN_EXPIRES_IN",
-                    ],
-                },
-                NestAuthJwtStrategy,
-                NestAuthGoogleStrategy,
-                NestAuthFacebookStrategy,
-                JwtSecretProvider,
-                JwtExpiresInProvider,
-                JwtRefreshTokenExpiresInProvider,
-                LocalStrategy,
-                LocalGuard,
-
-                // {
-                //     provide: APP_FILTER,
-                //     useClass: HttpExceptionFilter,
-                // },
-            ],
-            exports: [nestAuthServiceToken],
-            controllers: [controller],
-        };
-    }
+        // {
+        //     provide: APP_FILTER,
+        //     useClass: HttpExceptionFilter,
+        // },
+      ],
+      exports: [nestAuthServiceToken],
+      controllers: [controller],
+    };
+  }
 }
